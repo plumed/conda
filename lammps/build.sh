48da131f37ab5db13919a38c46efb7be1f6a5b6b
Giovanni Bussi <giovanni.bussi@gmail.com>
2019-07-04 09:19:22 +0200

    test lammps

---
+++ lammps/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# Serial
+mkdir build_serial
+cd build_serial
+cmake -D BUILD_MPI=OFF -D BUILD_OMP=OFF -D PKG_PLUMED=ON -D PLUMED_MODE=runtime  ../cmake
+make -j${CPU_COUNT}
+cp lmp $PREFIX/bin/lmp_serial
+cd ..
