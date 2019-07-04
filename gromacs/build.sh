2576fc0c826c19500f9f3b3c594d5858f46efa51
Giovanni Bussi <giovanni.bussi@gmail.com>
2019-07-04 07:02:10 +0200

    fixes

--- gromacs/build.sh
+++ gromacs/build.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 set -e
-plumed patch -e gromacs-2019.2
+plumed-patch -p --runtime -e gromacs-2018.6
 mkdir build
 cd build
+# GMX_LIB_INSTALL_DIR=lib - make sure library is installed in lib and not lib64
 cmake .. \
-  -DGMX_BUILD_OWN_FFTW=ON \
   -DGMX_DEFAULT_SUFFIX=ON \
   -DGMX_MPI=OFF \
   -DGMX_GPU=OFF \
   -DGMX_SIMD=SSE2 \
   -DCMAKE_PREFIX_PATH=$PREFIX \
-  -DGMX_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_INSTALL_PREFIX=$PREFIX
 make -j $CPU_COUNT
 make install
