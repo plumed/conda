140133d40693fc9d2771fbc414acba23fcf7cc16
Giovanni Bussi <giovanni.bussi@gmail.com>
2019-07-04 09:53:45 +0200

    fix

--- lammps/build.sh
+++ lammps/build.sh
@@ -4,7 +4,7 @@ set -e
 # Serial
 mkdir build_serial
 cd build_serial
-cmake -D BUILD_MPI=OFF -D BUILD_OMP=OFF -D PKG_USER-PLUMED=ON -D PLUMED_MODE=runtime  ../cmake
+cmake -D BUILD_MPI=OFF -D BUILD_OMP=OFF -D PKG_USER-PLUMED=ON -D PLUMED_MODE=runtime ../cmake
 make -j${CPU_COUNT}
 cp lmp $PREFIX/bin/lmp_serial
 cd ..
