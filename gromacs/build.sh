2b2c34f590ffea8b8afb49bca2ba743b5e44e2cd
Giovanni Bussi <giovanni.bussi@gmail.com>
2019-07-05 11:34:32 +0200

    xx

--- gromacs/build.sh
+++ gromacs/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 
-ls -l /Library/Developer/CommandLineTools/SDKs/ || echo not found
-ls -l /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/ || echo not found
+find /Applications/Xcode.app -name MacOKSX.sdk
 
 plumed-patch -p --runtime -e gromacs-2018.6
 mkdir build
@@ -12,7 +11,7 @@ cmake .. \
   -DGMX_MPI=OFF \
   -DGMX_GPU=OFF \
   -DGMX_SIMD=SSE2 \
-  -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MaxOSX.sdk \
+  -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk \
   -DCMAKE_PREFIX_PATH=$PREFIX \
   -DCMAKE_INSTALL_PREFIX=$PREFIX
 make -j $CPU_COUNT
