2eafb490f00f3708d1a7827c0fdb4967ca0be341
Giovanni Bussi <giovanni.bussi@gmail.com>
2019-07-05 10:13:44 +0200

    fix lammps

--- lammps/build.sh
+++ lammps/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
-# Serial
-mkdir build_serial
-cd build_serial
-cmake -D BUILD_MPI=OFF -D BUILD_OMP=OFF -D PKG_USER-PLUMED=ON -D PLUMED_MODE=runtime ../cmake
-make -j${CPU_COUNT}
-cp lmp $PREFIX/bin/lmp_serial
-cd ..
+cd src
+make lib-plumed args="-p $PREFIX -m runtime"
+make yes-kspace
+make yes-molecule
+make yes-rigid
+make yes-manybody
+make yes-user-plumed
+make -j${CPU_COUNT} serial
+cp lmp_serial $PREFIX/bin/lmp_serial
