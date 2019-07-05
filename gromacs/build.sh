ae3b2df2ae9a53599a899c1c75532abad51a4b23
Giovanni Bussi <giovanni.bussi@gmail.com>
2019-07-05 11:19:29 +0200

    sysroot

--- gromacs/build.sh
+++ gromacs/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
+
+ls -l /Library/Developer/CommandLineTools/SDKs/ || echo not found
+ls -l /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/ || echo not found
+
 plumed-patch -p --runtime -e gromacs-2018.6
 mkdir build
 cd build
@@ -8,7 +12,7 @@ cmake .. \
   -DGMX_MPI=OFF \
   -DGMX_GPU=OFF \
   -DGMX_SIMD=SSE2 \
-  -DCMAKE_INSTALL_RPATH:PATH="${PREFIX}/lib" \
+  -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MaxOSX.sdk \
   -DCMAKE_PREFIX_PATH=$PREFIX \
   -DCMAKE_INSTALL_PREFIX=$PREFIX
 make -j $CPU_COUNT
