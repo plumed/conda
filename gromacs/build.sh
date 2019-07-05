ffef5f0dcb827a2bdea0eaf5605450a0ba7fdfd4
Giovanni Bussi <giovanni.bussi@gmail.com>
2019-07-05 11:52:06 +0200

    maybe fixed..

--- gromacs/build.sh
+++ gromacs/build.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 set -e
 
-find /Applications/Xcode.app -name MacOSX.sdk
+opt=""
 
-exit 1
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  opt="-DCMAKE_OSX_SYSROOT=$(xcrun --show-sdk-path)"
+fi
 
 plumed-patch -p --runtime -e gromacs-2018.6
 mkdir build
 cd build
 cmake .. \
+  $opt \
   -DGMX_DEFAULT_SUFFIX=ON \
   -DGMX_MPI=OFF \
   -DGMX_GPU=OFF \
   -DGMX_SIMD=SSE2 \
-  -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk \
   -DCMAKE_PREFIX_PATH=$PREFIX \
   -DCMAKE_INSTALL_PREFIX=$PREFIX
 make -j $CPU_COUNT
