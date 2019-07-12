44cfe44a10b3f28c2d11e38a4e67c341ab7d7bea
Giovanni Bussi <giovanni.bussi@gmail.com>
2019-07-12 17:49:51 -0600

    sed

--- lammps/build.sh
+++ lammps/build.sh
@@ -5,10 +5,15 @@ cat src/USER-PLUMED/fix_plumed.cpp | grep -v setMPIComm > src/USER-PLUMED/fix_pl
 mv src/USER-PLUMED/fix_plumed.cpp.fix src/USER-PLUMED/fix_plumed.cpp
 
 if [[ $(uname) == Darwin ]]; then
+echo "A"
   cat cmake/Modules/Packages/USER-PLUMED.cmake | sed "s/libplumedKernel.so/libplumedKernel.dylib/" > cmake/Modules/Packages/USER-PLUMED.cmake.fix
+echo "A"
   mv cmake/Modules/Packages/USER-PLUMED.cmake.fix cmake/Modules/Packages/USER-PLUMED.cmake
+echo "A"
   cat lib/plumed/Makefile.lammps.runtime | sed "s/libplumedKernel.so/libplumedKernel.dylib/" > lib/plumed/Makefile.lammps.runtime.fix
+echo "A"
   mv lib/plumed/Makefile.lammps.runtime.fix lib/plumed/Makefile.lammps.runtime
+echo "A"
 fi
 
 cd src
