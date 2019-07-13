29bb4b26ddf75510c6dd76667f14f3f77e6205f6
Giovanni Bussi <giovanni.bussi@gmail.com>
2019-07-12 20:21:03 -0600

    better

--- lammps/build.sh
+++ lammps/build.sh
@@ -5,12 +5,16 @@ set -e
 cat src/USER-PLUMED/fix_plumed.cpp | grep -v setMPIComm > src/USER-PLUMED/fix_plumed.cpp.fix
 mv src/USER-PLUMED/fix_plumed.cpp.fix src/USER-PLUMED/fix_plumed.cpp
 
+opt=""
+
 # Fix the name of the PLUMED kernel on OSX
 if [[ $(uname) == Darwin ]]; then
   cat cmake/CMakeLists.txt | sed "s/libplumedKernel.so/libplumedKernel.dylib/" > cmake/CMakeLists.txt.fix
   mv cmake/CMakeLists.txt.fix cmake/CMakeLists.txt
   cat lib/plumed/Makefile.lammps.runtime | sed "s/libplumedKernel.so/libplumedKernel.dylib/" > lib/plumed/Makefile.lammps.runtime.fix
   mv lib/plumed/Makefile.lammps.runtime.fix lib/plumed/Makefile.lammps.runtime
+# CMAKE_OSX_SYSROOT and CMAKE_OSX_DEPLOYMENT_TARGET should be set for cmake to work correctly
+  opt="-DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT} -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}"
 fi
 
 LIBS="$LIBS -llapack -lblas"
@@ -25,6 +29,7 @@ cmake -DBUILD_MPI=no \
       -DPKG_MOLECULE=yes \
       -DPKG_RIGID=yes \
       -DPKG_USER-PLUMED=yes \
+      $opt \
       ../cmake
 make VERBOSE=1 -j${CPU_COUNT}
 
