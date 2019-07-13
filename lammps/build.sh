43e7e19ae7b64c12a86a47eaead49a7f390c5e9d
Giovanni Bussi <giovanni.bussi@gmail.com>
2019-07-12 20:03:19 -0600

    cmake

--- lammps/build.sh
+++ lammps/build.sh
@@ -7,16 +7,31 @@ mv src/USER-PLUMED/fix_plumed.cpp.fix src/USER-PLUMED/fix_plumed.cpp
 
 # Fix the name of the PLUMED kernel on OSX
 if [[ $(uname) == Darwin ]]; then
+  cat cmake/CMakeLists.txt | sed "s/libplumedKernel.so/libplumedKernel.dylib/" > cmake/CMakeLists.txt.fix
+  mv cmake/CMakeLists.txt.fix cmake/CMakeLists.txt
   cat lib/plumed/Makefile.lammps.runtime | sed "s/libplumedKernel.so/libplumedKernel.dylib/" > lib/plumed/Makefile.lammps.runtime.fix
   mv lib/plumed/Makefile.lammps.runtime.fix lib/plumed/Makefile.lammps.runtime
 fi
 
-cd src
-make lib-plumed args="-p $PREFIX -m runtime"
-make yes-kspace
-make yes-molecule
-make yes-rigid
-make yes-manybody
-make yes-user-plumed
-make -j${CPU_COUNT} serial
+make -C src lib-plumed args="-p $PREFIX -m runtime"
+
+mkdir build
+cd build
+cmake -DBUILD_MPI=no \
+      -DPKG_MANYBODY=yes \
+      -DPKG_KSPACE=yes \
+      -DPKG_MOLECULE=yes \
+      -DPKG_RIGID=yes \
+      -DPKG_USER-PLUMED=yes \
+      ../cmake
+make VERBOSE=1 -j${CPU_COUNT}
+
+# cd src
+# make yes-kspace
+# make yes-molecule
+# make yes-rigid
+# make yes-manybody
+# make yes-user-plumed
+# make -j${CPU_COUNT} serial
+
 cp lmp_serial $PREFIX/bin/lmp_serial
